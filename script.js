badc60369fa8c307d4e8b34a0752ed2eccffe208
NoshalFatima <160382265+NoshalFatima@users.noreply.github.com>
2025-08-28 17:50:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ function renderItems(category) {
 
   items.forEach(item => {
     const html = `
-      <div class="rounded-lg overflow-hidden shadow-md h-[420px] lg:ml-10 lg:mr-6 p-2 hover:shadow-xl border-t-[32px] border-l-[32px] border-r-[32px] border-[#170C24] transition bg-white lg:w-[500px] w-full group cursor-pointer  relative">
+      <div class="rounded-lg overflow-hidden shadow-md h-[420px] lg:ml-10 lg:mr-6  hover:shadow-xl border-t-[32px] border-l-[32px] border-r-[32px] border-[#170C24] transition bg-white lg:w-[480px] w-full group cursor-pointer  relative">
           
         <img src="${item.image}" class="portfolio-bg h-full  w-full " style="
           
@@ -469,6 +469,7 @@ function downloadFile(filename) {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);}
+
 
 
 
